30fc70127e6eb8642f4d09b87c731b937a255269
kittrz <kittrz@kittrz.gay>
2024-05-07 13:18:59 -0500

    fix object name not changing the asm extension

--- build.sh
+++ build.sh
@@ -27,9 +27,10 @@ CFILES="$(find src/ -name "*.asm")"
 OBJS=""
 
 for f in $CFILES; do
-	OBJNAME="$(echo "$f" | sed -e "s/\.s/\.o/" -e "s/src/obj/")"
+	OBJNAME="$(echo "$f" | sed -e "s/\.asm/\.o/" -e "s/src/obj/")"
 	$ASM -felf64 "$f" -o "$OBJNAME"
 	OBJS="$OBJS $OBJNAME"
 done
 
 $LD $OBJS -o build/shell -N
+
